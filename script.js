659829f912f2f4dc6e351580475074caeb2b6f9f
Lucas Z Lipreri <Z.lucas.lipreri@gmail.com>
2023-10-11 20:20:40 -0300

    change back ground based on media queries

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("light")) {
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute("src", "./assets/profile//avatar-light.png")
   } else {
-       img.setAttribute("src", "./assets/profile/avatar.png")
+    img.setAttribute("src", "./assets/profile/avatar.png")
   }
 }
